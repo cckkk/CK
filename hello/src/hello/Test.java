9e681a563451224ebcb70572ad48ee4e4d26e610
callab <callab@ENI-B9-14>
2015-11-04 18:23:51 +0900

    현재 시간 출력을 추가했습니다. -정태민

--- hello/src/hello/Test.java
+++ hello/src/hello/Test.java
@@ -1,8 +1,21 @@
 package hello;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		System.out.println("Current Time is : " + getTime());
+	}
+
+	private static String getTime() {
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+		
+		String formattedDate = dateFormat.format(date);
+		// TODO Auto-generated method stub
+		return formattedDate;
 	}
 
 }
